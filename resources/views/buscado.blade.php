c43d4c24c7ec7b7091e79d4723c49e61551cad58
marcos <marks88martinez@gmail.com>
2016-05-12 12:12:53 -0300

    mejor

--- resources/views/buscado.blade.php
+++ resources/views/buscado.blade.php
@@ -14,15 +14,15 @@
 
        <div class="container">
            @if (count($articles) === 0)
-               <div class="fonte2"> <h2>  ...Sin Resultados</h2></div>
+               <div class="fonte2"> <h2>  ...Sim Resultados</h2></div>
            @elseif (count($articles) >= 1)
-               <div class="fonte2"> <h2>  ... Resultado de Búsqueda:</h2></div>
+               <div class="fonte2"> <h2>  ... Resultado da Busca:</h2></div>
 
 
                <ul>
                    @foreach($articles as $article)
                        <hr>
-                       <li> <a href="productos/{{ $article->codigo_producto}}"><img class="ima" src="{{URL::to($article->imagenes->url_imagenes)}}" alt=""> {{ $article->nombre_producto}}</a></li>
+                       <li> <a href="{{ $article->codigo_producto}}"><img class="ima" src="{{URL::to($article->imagenes->url_imagenes)}}" alt=""> {{ $article->nombre_producto}}</a></li>
                    @endforeach
                </ul>
 
--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -612,7 +612,7 @@
 
 {{--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>--}}
 {{--<script>window.jQuery || document.write('<script src="{{URL::to('vista/new_prod/js/jquery-1.10.2.min.js')}}"><\/script>')</script>--}}
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+{{--<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>--}}
 {{--<script src="{{URL::to('vista/new_prod/js/bootstrap.min.js')}}"></script>--}}
 <script src="{{URL::to('vista/new_prod/js/jquery.slimscroll.min.js')}}"></script>
 <script src="{{URL::to('vista/new_prod/js/jquery.fullPage.min.js')}}"></script>
