01a1bcb36c00bbf610b51ef0351b7b0cb05830a5
marcos <marks88martinez@gmail.com>
2016-05-20 12:41:17 -0300

    cont

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -7,23 +7,23 @@
         <h4 class=" gris fonte">•  Contato</h4>
         <br>
 
-        <form class="form-horizontal" role="form" method="post" action="index.php">
+        <form class="form-horizontal" role="form" method="post" action="/">
             <div class="form-group">
                 <label for="name" class="col-sm-2 control-label">Nome</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="" required>
                 </div>
             </div>
             <div class="form-group">
                 <label for="email" class="col-sm-2 control-label">Email</label>
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="" required>
                 </div>
             </div>
             <div class="form-group">
                 <label for="message" class="col-sm-2 control-label">Mensagem</label>
                 <div class="col-sm-10">
-                    <textarea class="form-control" rows="4" name="message"></textarea>
+                    <textarea class="form-control" rows="4" name="message" required></textarea>
                 </div>
             </div>
             {{--<div class="form-group">--}}
